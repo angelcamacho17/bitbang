016ce3b4ec9918134ef8dd22e444fb0201d36600
angelcamacho17 <angelcamacho1708@gmail.com>
2022-12-28 16:28:36 -0400

    build

---
+++ build/precache-manifest.aadeaa1325770032b853893bdd5a1c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36b47d934cea7cd66080",
+    "url": "/static/css/main.f9230c22.chunk.css"
+  },
+  {
+    "revision": "36b47d934cea7cd66080",
+    "url": "/static/js/main.7daed62a.chunk.js"
+  },
+  {
+    "revision": "44379f9d8a742a556af9",
+    "url": "/static/js/runtime~main.1b973b83.js"
+  },
+  {
+    "revision": "8dc880bdb414e675770f",
+    "url": "/static/js/2.8226ca1c.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "92515ff54b1ba1bf652ab534af4acf60",
+    "url": "/index.html"
+  }
+];
